dd13d1d32463498eaaf1ac2dfa4b5c584a7ffdd0
udayakumar-uk <v.udayakumar97@gmail.com>
2025-09-17 20:21:47 +0530

    Tobacco React Project

---
+++ src/index.js
@@ -0,0 +1,35 @@
+// scroll bar
+import 'simplebar/src/simplebar.css';
+
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+
+//
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
+
+import { AuthProvider } from './context/AuthContext';
+
+// ----------------------------------------------------------------------
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <HelmetProvider>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
+  </HelmetProvider>
+);
+
+// If you want to enable client cache, register instead.
+serviceWorker.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ src/sections/auth/login/LoginForm.js
@@ -0,0 +1,110 @@
+import * as Yup from 'yup';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+// form
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+// @mui
+import { Stack, IconButton, InputAdornment, Alert } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+// components
+import Iconify from '../../../components/Iconify';
+import { FormProvider, RHFTextField } from '../../../components/hook-form';
+
+// Auth Context
+import { useAuth } from '../../../context/AuthContext';
+
+// ----------------------------------------------------------------------
+
+export default function LoginForm() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState("");
+
+  const { login, user } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      navigate("/dashboard");
+    }
+  }, [user, navigate]);
+
+  const LoginSchema = Yup.object().shape({
+    email: Yup.string()
+      .email('Email must be a valid email address')
+      .required('Email is required'),
+    password: Yup.string().required('Password is required'),
+  });
+
+  const defaultValues = {
+    email: '',
+    password: '',
+  };
+
+  const methods = useForm({
+    resolver: yupResolver(LoginSchema),
+    defaultValues,
+  });
+
+  const {
+    handleSubmit,
+    formState: { isSubmitting },
+  } = methods;
+
+  const onSubmitForm = async (formData) => {
+    try {
+      const data = await login(formData);
+      
+      if (data.token) {
+        navigate('/dashboard', { replace: true });
+        setError('');
+      } else {
+        setError(data.response || 'Login failed');
+      }
+    } catch (err) {
+      setError("Invalid credentials");
+    }
+  };
+
+  return (
+    <>
+      {error && (
+        <Alert severity="error" variant="filled" sx={{ my: 4 }}>
+          {error}
+        </Alert>
+      )}
+
+      <FormProvider methods={methods} onSubmit={handleSubmit(onSubmitForm)}>
+        <Stack spacing={3}>
+          <RHFTextField name="email" label="Email address" />
+
+          <RHFTextField
+            name="password"
+            label="Password"
+            type={showPassword ? 'text' : 'password'}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
+                    <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Stack>
+
+        <LoadingButton
+          sx={{ my: 3 }}
+          fullWidth
+          size="large"
+          type="submit"
+          variant="contained"
+          loading={isSubmitting}
+        >
+          Login
+        </LoadingButton>
+      </FormProvider>
+    </>
+  );
+}
